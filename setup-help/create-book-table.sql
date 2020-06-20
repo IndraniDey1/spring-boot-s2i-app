68ae1ff2cd2e63874df084ccd67906ee705c255c
Indrani Dey <indrani.dey1@fmc-na.com>
2020-06-20 12:59:44 -0400

    changed filenames

---
+++ setup-help/create-book-table.sql
@@ -0,0 +1,15 @@
+USE bookstore;
+
+DROP TABLE IF EXISTS book;
+
+CREATE TABLE IF NOT EXISTS `book` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `author` varchar(255) DEFAULT NULL,
+  `category` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `pages` int(11) DEFAULT NULL,
+  `price` int(11) DEFAULT NULL,
+  `publication` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+~                                                          
---
+++ setup-help/mysql-setup-for-s2i-app.bat
@@ -0,0 +1,21 @@
+set mpod=mysql-1-cj68h
+echo "Copying setup files into pod..."
+oc cp create-book-table.sql %mpod%:/tmp/create-book-table.sql
+oc cp insert-book-data.sql %mpod%:/tmp/insert-book-data.sql
+
+echo "Creating table(s)..."
+oc exec %mpod% -- bash -c "mysql --user=root bookstore < /tmp/create-book-table.sql"
+
+echo "Importing data..."
+oc exec %mpod% -- bash -c "mysql --user=root < /tmp/insert-book-data.sql"
+
+echo "Here is your table:"
+oc exec %mpod% -- bash -c "mysql --user=root bookstore -e 'use bookstore; SELECT * FROM book;'"
+
+
+echo "Granting privileg to user1..."
+oc exec %mpod% -- bash -c "mysql --user=root -e 'GRANT ALL PRIVILEGES ON *.* TO '\''user1'\''@'\''%'\'' ;'"
+
+echo "All done with database setup !!!!"
+
+

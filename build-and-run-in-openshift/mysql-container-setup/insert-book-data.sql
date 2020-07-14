7b1b22f73fe19a48051b4e97c839b01c6ce6a6fe
IndraniDey1 <IndraniDey@live.com>
2020-07-14 08:37:15 -0400

    Update insert-book-data.sql

--- build-and-run-in-openshift/mysql-container-setup/insert-book-data.sql
+++ build-and-run-in-openshift/mysql-container-setup/insert-book-data.sql
@@ -1,6 +1,6 @@
 USE bookstore;
-INSERT INTO `book` (`id`, `author`, `category`, `name`, `pages`, `price`, `publication`) VALUES
-(1, 'Santosh Kumar', 'Programming', 'Extensive Multithreading', 100, 50, 'Mcgraw Publication'),
-(3, 'Suchitra', 'Programming', 'Java', 1000, 200, 'Mcgraw Publication'),
-(5, 'Nitesh', 'Computer Programming', 'C++', 1500, 240, 'Rajput Publication'),
-(7, 'Indrani', 'Sample Book', 'C++', 1500, 240, 'ORiley Publication');
+INSERT INTO `book` (`id`, `author`, `category`, `name`, `pages`, `price`, `publication`,`year`) VALUES
+(1, 'Robert Smith', 'Programming', 'Extensive Multithreading', 100, 50, 'Mcgraw Publication',`2016`),
+(3, 'Suchitra Sharma', 'Programming', 'Java', 1000, 200, 'Mcgraw Publication',`2017`),
+(5, 'James Collins', 'Computer Programming', 'Quarkus', 1500, 240, 'Rajput Publication', `2019`),
+(7, 'Reggie Picaso', 'Sample Book', 'Vertx', 1500, 240, 'ORiley Publication',`2020`);

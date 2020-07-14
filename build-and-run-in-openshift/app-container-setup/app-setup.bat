8109239ef98c24bdf3a59e75e7c1f1cefa6e90e2
Indrani Dey <indrani.dey1@fmc-na.com>
2020-07-13 22:20:45 -0400

    added bat file

---
+++ build-and-run-in-openshift/app-container-setup/app-setup.bat
@@ -0,0 +1,9 @@
+create -f s2i-app.cm
+create -f s2i-app.sec
+oc new-app java8~https://github.com/IndraniDey1/spring-boot-s2i-app.git --name s2i-app
+
+
+oc set env dc/s2i-app --from configmap/s2i-app.cm
+oc set env dc/s2i-app --from secret/s2i-app.sec
+oc rollout latest dc/s2i-app
+
